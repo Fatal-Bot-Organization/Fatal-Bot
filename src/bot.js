18b99cbdcc83ea92d6d3179c611593ccf9c14b50
Project Void <fearlesstechofficial@gmail.com>
2023-08-30 00:46:52 -0500

    Added 3 commands. Added a temp registor event.

--- src/bot.js
+++ src/bot.js
@@ -16,10 +16,22 @@ client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on(Events.MessageCreate, async message => {
-	if (message.content === '!ping') {
-		await message.reply('Pong!');
-	}
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+  
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+  if (interaction.commandName === "credits") {
+    await interaction.reply("Main Developer: <@!1120137191557963806>\nHelper: <@!894783593694527499> ");
+  }
+  if (interaction.commandName === "add") {
+    const num1 = interaction.options.getNumber("first-number").value;
+    const num2 = interaction.options.getNumber("second-number").value;
+
+    await interaction.reply(`The sum is ${num1 + num2}`);
+  }
+
 });
 
 client.login(token);
---
+++ src/register.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const { token, clientId } = process.env;
+const { REST, Routes, ApplicationCommandOptionType } = require("discord.js");
+
+const commands = [
+  {
+	name: "ping",
+	description: "Replies with Pong!",
+  },
+  {
+	name: "credits",
+	description: "Shows the credits of the bot",
+  },
+  {
+	name: "add",
+	description: "Adds two numbers",
+	options: [
+		{
+			name: "first-number",
+			description: "The first number to add",
+			type: ApplicationCommandOptionType.Number,
+			required: true
+		},
+		{
+			name: "second-number",
+			description: "The Second number to add",
+			type: ApplicationCommandOptionType.Number,
+			required: true
+		},
+	],
+  },
+];
+
+const rest = new REST().setToken(token);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
